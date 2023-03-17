4882c1747659e90133788e263b143ee75526d104
LucasRB7 <97030780+LucasRB7@users.noreply.github.com>
2023-03-17 15:00:26 -0300

    Atualizacao index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,31 @@
 const botoes = document.querySelectorAll(".botao");
-botoes.forEach((botao, indice) => {
+const imagens = document.querySelectorAll(".imagem");
+
+botoes.forEach((botao, index) => {
     botao.addEventListener("click", () => {
-
+       
+        desativarBotaoSelecionado();
+        ativarBotaoSelecionado(botao);
+        desativarImagemDeFundo();
+        ativarImagemDeFundo(index);
+        
     })
-
-    
-
     
 });
 
-
-
+function desativarBotaoSelecionado(){
+    const botaoSelecionado = document.querySelector(".selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+function ativarBotaoSelecionado(botao){
+    
+    botao.classList.add("selecionado"); 
+}
+function desativarImagemDeFundo(){
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+        
+}
+function ativarImagemDeFundo(index){
+    imagens[index].classList.add("ativa");
+}
